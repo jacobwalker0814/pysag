a9d768e037dbce817a31d5aaa5e90379014da135
Jacob Walker <jwalker@tortugas-llc.com>
2014-09-09 08:40:25 -0500

    First test working

--- pysag/reader.py
+++ pysag/reader.py
@@ -1,3 +1,26 @@
+import os
+import glob
+import yaml
+
+
 class Reader:
-    def read(self):
-        return True
+    def read(self, data_dir):
+        # Walk through the directory {data_dir}. Any folders there become top
+        # level elements in our data. Any .yml files there will be parsed into
+        # results
+
+        data = {}
+        for data_type in os.listdir(data_dir):
+            data[data_type] = []
+            for file_name in glob.glob('%s/%s/*' % (data_dir, data_type)):
+                # Read the file as YAML
+                f = open(file_name, 'r')
+                datum = yaml.load(f)
+                f.close()
+
+                # The file's basename is its id
+                basename = os.path.splitext(os.path.basename(file_name))[0]
+                datum['id'] = basename
+                data[data_type].append(datum)
+
+        return data
--- pysag/test/reader.py
+++ pysag/test/reader.py
@@ -1,22 +1,29 @@
 import unittest
+import os
 from .. import Reader
 
 
 class ReaderTest(unittest.TestCase):
     def setUp(self):
         self.reader = Reader()
+        self.fixture_base = os.path.dirname(__file__)
 
     def test_read_simple_single(self):
         expected = {
-            "users": [
+            'users': [
                 {
-                    "id": 1,
-                    "name": "Kell"
+                    'id': '1',
+                    'name': 'Kell'
                 }
             ]
         }
-        result = self.reader.read('./fixtures/simple_single')
+        fixture_path = self.fixture_base + '/fixtures/simple_single'
+        result = self.reader.read(fixture_path)
         self.assertEqual(result, expected)
+
+    # TODO read only yml
+    # TODO read multiple
+    # TODO test failure cases
 
 if __name__ == '__main__':
     unittest.main()
